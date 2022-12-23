fa760340ed847b5be533c654c5edd7a4928c8fa3
Ammar Sakka <117344965+ammarsakka@users.noreply.github.com>
2022-12-23 09:38:45 +0800

    main

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <p className='text-4xl text-black font-bold'>
+        Hello World
+      </p>
+    </div>
+  );
+}
